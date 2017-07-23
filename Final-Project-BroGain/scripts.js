b61f630235848951b1ef68e727f21c00a4dad2c4
Oliver Gillette <olivergillette@gmail.com>
2017-07-23 10:36:27 -0400

    Updates to Brogain
    Getting closer to connecting Firebase and Highcharts, found out how to store the data, now trying to figure out the format highcharts wants

--- Final-Project-BroGain/scripts.js
+++ Final-Project-BroGain/scripts.js
@@ -1,81 +1,158 @@
 $(document).ready(function(){
 
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAYDf6T7nUmhDLmw3yZ5wX0TQbvJZJM21E",
-    authDomain: "brogain-e4808.firebaseapp.com",
-    databaseURL: "https://brogain-e4808.firebaseio.com",
-    projectId: "brogain-e4808",
-    storageBucket: "brogain-e4808.appspot.com",
-    messagingSenderId: "1009650252599"
-  };
-  
-  firebase.initializeApp(config);
+  // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAYDf6T7nUmhDLmw3yZ5wX0TQbvJZJM21E",
+      authDomain: "brogain-e4808.firebaseapp.com",
+      databaseURL: "https://brogain-e4808.firebaseio.com",
+      projectId: "brogain-e4808",
+      storageBucket: "brogain-e4808.appspot.com",
+      messagingSenderId: "1009650252599"
+    };
+    firebase.initializeApp(config);
+    var brogainDataReference = firebase.database();
 
-  var firebaseAppReference = firebase.database();
+  // Log the weight and reps? of a workout
+    $('#workout-form').submit(function(event){
+      event.preventDefault();
+      var workout1Weight = $('#workout1').val();
+      $('#workout1').val('');
+      var workout2Weight = $('#workout2').val();
+      $('#workout2').val('');
+      var workout3Weight = $('#workout3').val();
+      $('#workout3').val('');
+      var workout4Weight = $('#workout4').val();
+      $('#workout4').val('');
+      var workout5Weight = $('#workout5').val();
+      $('#workout5').val('');
+      var workout6Weight = $('#workout6').val();
+      $('#workout6').val('');
 
-  var fakeInput1 = {
-  	input1: 15,
-  	input2: 15,
-  	input3: 20,
-  	input4: 28,
-  	input5: 35,
-  	input6: 45,
-  }
+      var workout1Reference = brogainDataReference.ref('Workout-1-Table');
+      var workout2Reference = brogainDataReference.ref('Workout-2-Table');
+      var workout3Reference = brogainDataReference.ref('Workout-3-Table');
+      var workout4Reference = brogainDataReference.ref('Workout-4-Table');
+      var workout5Reference = brogainDataReference.ref('Workout-5-Table');
+      var workout6Reference = brogainDataReference.ref('Workout-6-Table');
 
-
-
-
-
-
-    // $('#workout-form').submit(function(event){
-    //       event.preventDefault();
-    //       var workoutInput = $('#')
-
-    $('#testerz').Highcharts.chart('container', {
-
-          title: {
-              text: 'Solar Employment Growth by Sector, 2010-2016'
-          },
-
-          subtitle: {
-              text: 'Source: thesolarfoundation.com'
-          },
-
-          yAxis: {
-              title: {
-                  text: 'Number of Employees'
-              }
-          },
-          legend: {
-              layout: 'vertical',
-              align: 'right',
-              verticalAlign: 'middle'
-          },
-
-          plotOptions: {
-              series: {
-                  pointStart: 2010
-              }
-          },
-
-          series: [{
-              name: 'Installation',
-              data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-          }, {
-              name: 'Manufacturing',
-              data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-          }, {
-              name: 'Sales & Distribution',
-              data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-          }, {
-              name: 'Project Development',
-              data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-          }, {
-              name: 'Other',
-              data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-          }]
-
+      workout1Reference.push({
+        weight: workout1Weight,
+        repetitions: 0
+      });
+      workout2Reference.push({
+        weight: workout1Weight,
+        repetitions: 0
+      });
+      workout3Reference.push({
+        weight: workout1Weight,
+        repetitions: 0
+      });
+      workout4Reference.push({
+        weight: workout1Weight,
+        repetitions: 0
+      });
+      workout5Reference.push({
+        weight: workout1Weight,
+        repetitions: 0
+      });
+      workout6Reference.push({
+        weight: workout1Weight,
+        repetitions: 0
       });
     });
+
+  // Retreiving Data from Firebase
+    var dataRef = brogainDataReference.ref('Workout-1-Table')
+    dataRef.on('value', gotData, errData);
+
+
+    function gotData(data) {
+      // console.log(data.val());
+      var data = data.val();
+      var keys = Object.keys(data);
+      // console.log(keys);
+      for (var i = 0; i < keys.length; i++) {
+        var keyIndex = keys[i];
+        var weight = data[keyIndex].weight;
+        var repetitions = data[keyIndex].repetitions;
+        // console.log(weight,repetitions)
+      }
+    }
+
+    function errData(err) {
+      console.log("Error!");
+      console.log(err);
+    }
+
+
+  
+  // Create graph with Highcharts 
+    $('#display-graph').on('click', function(event){
+      event.preventDefault();
+      dataRef.on('value', function(data){
+        var data = data.val();
+        var keys = Object.keys(data);
+        // console.log(keys);
+        console.log(data);
+        for (var i = 0; i < keys.length; i++) {
+          var keyIndex = keys[i];
+          var weight = data[keyIndex].weight;
+          var repetitions = data[keyIndex].repetitions;
+          // console.log(weight,repetitions)
+        }
+      });
+      var url="https://brogain-e4808.firebaseio.com/.json";
+      $.getJSON(url, function(data){
+        var options = {
+          chart: {
+            renderTo: 'container',
+            marginTop: 0,
+            marginRight: 0,
+            marginLeft: 0,
+            spacing: [0,0,30,0],
+            type: 'area',
+            zoomType: 'xy',
+            events: {
+              // load: renderImage
+            }
+          },
+          legend: {
+            enabled: false
+          },
+          title: {
+            text: "Your very first Gain Graph!"
+          },
+          xAxis: {
+            type: 'linear'
+          },
+          yAxis: {
+            title: {
+              text: "Weight in Pounds"
+            },
+            plotlines: [{
+              value: 0,
+              width: 1,
+              color: '#808080'
+            }]
+          },
+          plotOptions: {
+            series: {
+              allowPointSelect: true
+            }
+          },
+          series: [{
+            name: workout1,
+            data: data
+          }]
+        }
+        var chart = new Highcharts.Chart(options);
+      });
+    });
+
+    
+
+
+
+
+});
 
