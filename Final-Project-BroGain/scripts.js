ae2954a09aef8cf1b7a2236cc6edbe96f6689602
Oliver Gillette <olivergillette@gmail.com>
2017-07-23 20:13:10 -0400

    This is the wack ass code Sonyl is giving me

--- Final-Project-BroGain/scripts.js
+++ Final-Project-BroGain/scripts.js
@@ -11,6 +11,10 @@ $(document).ready(function(){
     };
     firebase.initializeApp(config);
     var brogainDataReference = firebase.database();
+
+// Database Reference names 
+    var databaseReferences = [];   
+
     var chestRef = brogainDataReference.ref("Chest");
     var backRef = brogainDataReference.ref("Back");
     var armsRef = brogainDataReference.ref("Arms");
@@ -49,59 +53,72 @@ $(document).ready(function(){
       chestRef.push(chestDataRef);
     });
 
-  
+  var bodyRefData = {};
+  var bodyRef = brogainDataReference.ref('Body')
+  bodyRef.on('value', function(data){
+    bodyRefData = data.val();
+    for (var bodyArea in bodyRefData){
+      if (bodyRefData.hasOwnProperty(bodyArea)) {
+        databaseReferences.push(brogainDataReference.ref(bodyArea));
+      }
+    };
+    for (var i=0;i<databaseReferences.length;i++){
+      databaseReferences[i].on('value', function(data){
+      //data + input 1 ... 2 for second
+      })
+    }
+  });
+
   // Create graph with Highcharts 
     $('#display-graph').on('click', function(event){
       event.preventDefault();
       var dataRef = brogainDataReference.ref('Chest');
       dataRef.on('value', function(data){
-      var data = data.val();
-      var keys = Object.keys(data);
-      // console.log(data);
-      // console.log(keys);
-      var trueWeight = [];
-      for (var i = 0; i < keys.length; i++) {
-        var keyIndex = keys[i];
-        var weight = parseInt(data[keyIndex].dumbellpress);
-        trueWeight.push(weight);
-        console.log(weight)
-
-      };
-      var url="https://brogain-e4808.firebaseio.com/.json";
-      $.getJSON(url, function(data){
-        var options = {
-          chart: {
-            renderTo: 'container',
-            type: 'area',
-          },
-          title: {
-            text: "Your very first Gain Graph!"
-          },
-          xAxis: {
-            type: 'linear'
-          },
-          yAxis: {
-            title: {
-              text: "Weight in Pounds"
+        var data = data.val();
+        var keys = Object.keys(data);
+        // console.log(data);
+        // console.log(keys);
+        var trueWeight = [];
+        for (var i = 0; i < keys.length; i++) {
+          var keyIndex = keys[i];
+          var weight = parseInt(data[keyIndex].dumbellpress);
+          trueWeight.push(weight);
+        };
+        var url="https://brogain-e4808.firebaseio.com/.json";
+        $.getJSON(url, function(data){
+          var options = {
+            chart: {
+              renderTo: 'container',
+              type: 'area',
             },
-            plotlines: [{
-              value: 0,
-              width: 1,
-              color: '#808080'
+            title: {
+              text: "Your very first Gain Graph!"
+            },
+            xAxis: {
+              type: 'linear'
+            },
+            yAxis: {
+              title: {
+                text: "Weight in Pounds"
+              },
+              plotlines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+              }]
+            },
+            plotOptions: {
+              series: {
+                allowPointSelect: true
+              }
+            },
+            series: [{
+              name: workout1,
+              data: trueWeight
             }]
-          },
-          plotOptions: {
-            series: {
-              allowPointSelect: true
-            }
-          },
-          series: [{
-            name: workout1,
-            data: trueWeight
-          }]
-        }
-        var chart = new Highcharts.Chart(options);
-      });
+          }
+          var chart = new Highcharts.Chart(options);
+        });
       });
     });
 
